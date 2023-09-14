955fb0da55f28d1fba12cf1329329823cf4a838f
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-09-14 22:43:40 +0000

    setup code

---
+++ heap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int isHeap(int array[])
+{
+    int size = sizeof(array_name) / sizeof(array_name[index]);
+    
+    printf("%d", size);
+}
+
+
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 10};
+    isHeap(arr);
+
+    return 0;
+}
