948a8ba25c1d17b02a9f62329ee35df94afeeeee
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-09-16 02:14:43 +0000

    Update heap function

--- heap.c
+++ heap.c
@@ -5,9 +5,10 @@
 bool isHeap(int array[], int length)
 {
     for(int index = 0; index < length; index++){
-        if(2*index < length && array[2*index] > array[index])
+        // printf("%d left: %d right:%d\n", array[index], array[2*index+1], array[2*index+2]);
+        if(2*index + 1< length && array[2*index+1] > array[index])
             return false;
-        if(2*index + 1 < length && array[2*index+1] > array[index])
+        if(2*index + 2 < length && array[2*index+2] > array[index])
             return false;
 
     }
@@ -19,8 +20,11 @@ bool isHeap(int array[], int length)
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5, 10};
-    printf("%d", isHeap(arr, 6));    
+    //     10
+    //  5      6
+    // 4 5   1
+    int arr[] = {10, 5, 6, 4, 5, 1};
+    printf("%d\n", isHeap(arr, 5));    
     
     
     return 0;
