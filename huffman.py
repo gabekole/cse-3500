012ab629feb03cf454af82a603401129d0479a9f
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-10-20 04:07:24 +0000

    complete compression

--- huffman.py
+++ huffman.py
@@ -1,5 +1,8 @@
+import struct
+
+# Basically just calculates letter frequency
 def calculate_nucleotide_frequency(file_path):
-    result = {"C": 0, "T": 0, "G": 0, "A": 0}
+    result = {}
     total = 0.0
 
     with open(file_path, 'r') as file:
@@ -9,7 +12,10 @@ def calculate_nucleotide_frequency(file_path):
                 break
             else:
                 total += 1.0
-                result[char] += 1
+                if char not in result:
+                    result[char] = 1
+                else:
+                    result[char] += 1
 
     for key in result:
         result[key] /= total
@@ -17,14 +23,13 @@ def calculate_nucleotide_frequency(file_path):
     return result
 
 
-# ONLY VALID FOR 4 Symbols
 def huffman_coding(frequencies):
     # Freqs example: {'C': 0.11, 'T': 0.29, 'G': 0.11, 'A': 0.47}
 
     freqs = frequencies.copy()
 
 
-    codebook = {"C": [], "T": [], "G": [], "A": []}
+    codebook = {letter: [] for letter in freqs}
 
     while(len(freqs) > 1):
         in_order = sorted(freqs, key=lambda key: freqs[key])
@@ -47,11 +52,33 @@ def huffman_coding(frequencies):
         
     return codebook
 
+def encode_and_save_compressed(input_filepath, output_filepath):
+    freqs = calculate_nucleotide_frequency(input_filepath)
+    codebook = huffman_coding(freqs)
+
+    string_codebook = {letter : ''.join(map(str, code)) for letter, code in codebook.items()}
+
+    binary_data = []
+
+    with open(input_filepath, 'r') as input_file:
+        while True:
+            char = input_file.read(1)
+            if not char:
+                break
+            
+            binary_data.extend(codebook[char])
+
+    with open(output_filepath, "wb") as output_file:
+        # Pack the list of integers as binary data
+        binary_data_bytes = struct.pack("B" * len(binary_data), *binary_data) 
+        # The struct module is used to convert the integer
+        # 1s and 0s into bytes which can be written to the file
+    
+        # Write the binary data to the file
+        output_file.write(binary_data_bytes)
+
+    print(binary_data)
+
 
 if __name__ == "__main__":
-    freqs = calculate_nucleotide_frequency('./dna.txt')
-    
-    codebook = huffman_coding(freqs)
-    
-    print(freqs)
-    print(codebook)
+    encode_and_save_compressed('./dna.txt', 'compressed.txt')
